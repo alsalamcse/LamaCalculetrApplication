d8d5df93090fe59803e6528a841d892f7ea7d74b
user <hayam.mobarky@gmailcom>
2017-10-04 12:25:43 +0300

    hjkh

---
+++ app/src/main/java/com/example/user/calculetrapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.user.calculetrapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public abstract class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+
+
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+}
